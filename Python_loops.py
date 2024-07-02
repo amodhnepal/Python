a4d9ccb52f17f1e489d7116c3b47f8e64d4f3f0c
amodhnepal <nepalamodh8@gmail.com>
2024-07-02 21:57:37 +0545

    Learned for loop and range function also the concept of taking steps in range function

--- Python_loops.py
+++ Python_loops.py
@@ -1,5 +1,15 @@
 # Loops
-fruits=["Apple","Peach","Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit+ 'Pie')
+# fruits=["Apple","Peach","Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit+ 'Pie')
+    
+#  For loop with range
+
+# for number in range(1,11,3):
+#     print(number)
+
+total=0
+for number in range(1,101):
+    total+=number
+print(total)
