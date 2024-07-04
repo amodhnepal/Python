8430d44caf30a42745bc88497a6ca0f073baa2d8
amodhnepal <nepalamodh8@gmail.com>
2024-07-04 09:02:07 +0545

    Created a Hangman game

---
+++ Hangman.py
@@ -0,0 +1,108 @@
+import random
+from hangman_words import word_list
+
+stages = [
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """
+]
+
+end_of_game = False
+lives = 6
+
+display = []
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+    display += "_"
+
+print(display)
+
+while not end_of_game:
+    guess = input('Guess a letter: ').lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that is not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose")
+            print(f"The word was {chosen_word}")
+
+    print(" ".join(display))
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
+
+    print(stages[lives])
---
+++ hangman_words.py
@@ -0,0 +1,15 @@
+word_list = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+    "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yellowfruit",
+    "zucchini", "apricot", "blackberry", "blueberry", "cantaloupe", "dragonfruit",
+    "eggplant", "feijoa", "grapefruit", "huckleberry", "jackfruit", "kumquat", "lime",
+    "lychee", "mandarin", "mulberry", "olive", "passionfruit", "pear", "persimmon",
+    "pineapple", "plum", "pomegranate", "prune", "pumpkin", "radish", "rambutan",
+    "soursop", "starfruit", "tamarind", "tomato", "yam", "acorn", "almond", "avocado",
+    "bamboo", "beet", "broccoli", "cabbage", "carrot", "cauliflower", "celery", "chive",
+    "cucumber", "daikon", "dill", "endive", "fennel", "garlic", "ginger", "kale",
+    "lettuce", "mushroom", "okra", "onion", "parsley", "pea", "pepper", "potato",
+    "radicchio", "rutabaga", "shallot", "spinach", "squash", "sweetpotato", "turnip",
+    "wasabi", "watercress", "zucchini", "chard", "collard", "leek", "parsnip", "scallion"
+]
